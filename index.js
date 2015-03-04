c36754d423dd744605b8629518a99924ded4ef98
Luis Nell <luis@originell.org>
2015-03-04 15:06:22 +0100

    ready for deployment.

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 console.log("Ein JS Beispiel");
-console.log("Ist aber noch nicht fertig");
-
-alert("DO NOT USE IN PRODUCTION!");
-alert('BAD THINGS WILL HAPPEN!');
-
-document.body.style.backgroundColor = 'red';
-
-var yes = confirm("Are you sure you dont have this in production?!");
-if (!yes) {
-	window.close();
-}
